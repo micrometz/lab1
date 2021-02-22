d62f17d926a3ba57667234109241f9e61eb88b5d
Weston Metz <micrometz@gmail.com>
2021-02-21 17:23:16 -0700

    Add files via upload

---
+++ myclass.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+class MyClass
+{
+private:
+int counter;
+public:
+void Foo()
+{
+std::cout << "Foo" << std::endl;
+}
+void Foo() const
+{
+std::cout << "Foo const" << std::endl;
+}
+};
+int main()
+{
+MyClass cc;
+MyClass ccc;
+cc.Foo();
+ccc.Foo();
+}
